4b556ce808be573a766d0e683a2e3880945b48bb
Jacob <jacobtread@gmail.com>
2021-07-22 15:14:45 +1200

    Added scripts

---
+++ cloaks.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# The path to the linux host file
+HOSTS_FILE=/etc/hosts
+# The IP address for cloaks+
+IP="159.203.120.188"
+# The domain to redirect to cloaks+
+REDIRECT_DOMAIN="s.optifine.net"
+# The value to be inserted into the hosts file
+INSERT_VALUE="$IP $REDIRECT_DOMAIN"
+# The argument provided by the user
+OPTION=${1?You need to provide an option! please provide --install or --uninstall as an argument}
+
+if [ "$OPTION" = "--install" ]; then
+    if grep -q "$INSERT_VALUE" "$HOSTS_FILE"; then
+        echo "Cloaks+ is already installed!"
+    else
+        echo "Installing Cloaks+ into $HOSTS_FILE";
+        sudo -- sh -c -e "echo '$INSERT_VALUE' >> $HOSTS_FILE";
+        if grep -q "$INSERT_VALUE" "$HOSTS_FILE"; then
+            echo "Cloaks+ was successfully installed";
+        else
+            echo "Failed to install cloaks+"
+        fi
+    fi
+elif [ "$OPTION" = "--uninstall" ]; then
+    if grep -q "$INSERT_VALUE" "$HOSTS_FILE"; then
+        echo "Removing cloaks+"
+        sudo sed -i".bak" "/$INSERT_VALUE/d" $HOSTS_FILE
+        if grep -q "$INSERT_VALUE" "$HOSTS_FILE"; then
+            echo "Failed to remove cloaks+";
+        else
+            echo "Successfully removed cloaks+"
+        fi
+    else
+       echo "Cloaks+ is not installed 1"
+    fi
+else
+    echo "Unknown option please use '--install' or '--uninstall'."
+fi
